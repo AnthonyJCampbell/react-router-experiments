abde58e782eb607e83a26efb0a1a466221a4fb1b
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-09-03 07:06:33 +0200

    Adding copy for experimentation

---
+++ 2. Guides/src/App.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+// NESTED ROUTING
+// `/topics` loads <Topics> which then further conditionally renders any Routes that might be in place on that particular component, based on the paths `:id` value
+function Topic({ match }) {
+  console.log(`This is the match as part of a single <Topic>: `, match)
+  return <h3>Requested Param: {match.params.id}</h3>;
+}
+
+function Topics({ match }) {
+  // match is an object that passes on some info through the Link from Router
+  // It looks like this:
+  // {
+  //   isExact: false,
+  //   params: {},
+  //   path: "/topics",
+  //   url: "/topics",
+  // }
+
+  console.log(`This is the match as part of <Topics>: `, match)
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      {/* 
+        Here, we declare that when we go to `topics` / `:somevalue`, it should create a link that loads Topic 
+        In this case, `:id` is our declared param name, but could be named anything. It will show up in the match of <Topic> as:
+        params: {id: "PARAMSNAME"}
+      */}
+      <Route path={`${match.path}/:id`} component={Topic} />
+      {/* 
+        Essentially, this is a conditional rendering of the <h3>, since it only shows up when the URL is just `/topic` 
+        This is evident by the attribute of `path={match.path}`
+      */}
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/topics">Topics</Link>
+      </li>
+    </ul>
+  );
+}
+
+export default App;
